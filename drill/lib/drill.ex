79341e0529b280937ab8262ee2098790e761ac5f
thewaggletraining <thewaggletrainig@gmail.com>
2021-06-03 12:32:19 +0900

    add drill

---
+++ drill/lib/drill.ex
@@ -0,0 +1,18 @@
+defmodule Drill do
+  @moduledoc """
+  Documentation for `Drill`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Drill.hello()
+      "11Hello, World!"
+
+  """
+  def hello do
+    "Hello, World!"
+  end
+end
